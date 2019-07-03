8a34c67a9e64f7265069f7a5700ac1cbbb269ee9
Sean Brandt <sean@fuzzymagic.com>
2019-07-02 21:27:45 -0400

    Update and streamline

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,8 +1,4 @@
 #!/bin/sh
 alias reload!='exec "$SHELL" -l'
 
-alias ls='colorls --dark --sort-dirs'
-alias la='colorls --dark --sort-dirs -A'
-alias lc='colorls --tree --dark -A'
-
 alias top='htop'
