bf86ccdea14c4c548f1a1038a194aea2a599874c
Brandt, Sean (Contractor) <SBrandt@geico.com>
2019-07-24 07:37:05 -0400

    support ~/.zsh_custom

--- antibody/install.sh
+++ antibody/install.sh
@@ -2,4 +2,5 @@
 brew tap | grep -q 'getantibody/tap' || brew tap getantibody/tap
 brew install antibody
 antibody bundle <"$DOTFILES/antibody/bundles.txt" >~/.zsh_plugins.sh
+[[ -d ~/.zsh_custom ]] && antibody bundle ~/.zsh_custom >> ~/.zsh_plugins.sh
 antibody update
