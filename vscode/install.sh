8d37252302e470c0acc77bbaf85f1473eca6abd2
Brandt, Sean (Contractor) <SBrandt@geico.com>
2019-11-18 11:22:38 -0500

    support host specific configs

--- vscode/install.sh
+++ vscode/install.sh
@@ -1,15 +1,20 @@
 #!/bin/sh
 if command -v code >/dev/null; then
-	if [ "$(uname -s)" = "Darwin" ]; then
-		VSCODE_HOME="$HOME/Library/Application Support/Code"
-	else
-		VSCODE_HOME="$HOME/.config/Code"
-	fi
-	mkdir -p "$VSCODE_HOME/User"
+    if [ "$(uname -s)" = "Darwin" ]; then
+        VSCODE_HOME="$HOME/Library/Application Support/Code"
+    else
+        VSCODE_HOME="$HOME/.config/Code"
+    fi
+    mkdir -p "$VSCODE_HOME/User"
 
-	ln -snf "$DOTFILES/vscode/settings.json" "$VSCODE_HOME/User/settings.json"
-	ln -snf "$DOTFILES/vscode/keybindings.json" "$VSCODE_HOME/User/keybindings.json"
-	ln -snf "$DOTFILES/vscode/snippets" "$VSCODE_HOME/User/snippets"
+    if [ -d ${DOTFILES}/vscode/$(hostname) ]; then
+        ln -snf "$DOTFILES/vscode/$(hostname)/settings.json" "$VSCODE_HOME/User/settings.json"
+        ln -snf "$DOTFILES/vscode/$(hostname)/keybindings.json" "$VSCODE_HOME/User/keybindings.json"
+    else
+        ln -snf "$DOTFILES/vscode/settings.json" "$VSCODE_HOME/User/settings.json"
+        ln -snf "$DOTFILES/vscode/keybindings.json" "$VSCODE_HOME/User/keybindings.json"
+    fi
+    ln -snf "$DOTFILES/vscode/snippets" "$VSCODE_HOME/User/snippets"
 
 	while read -r module; do
 		code --install-extension "$module" || true
