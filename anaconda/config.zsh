dff91aceda7217b24805c32ccc20d2bcc82c69d5
Brandt, Sean (Contractor) <SBrandt@geico.com>
2019-11-20 09:05:41 -0500

    support local anaconda home override

--- anaconda/config.zsh
+++ anaconda/config.zsh
@@ -1,5 +1,7 @@
 #!/bin/zsh
 
+[[ -f ~/.anaconda_home ]] && source ~/.anaconda_home
+
 export ANACONDA_HOME="${ANACONDA_HOME:=/usr/local/anaconda3}"
 
 if [ -f "${ANACONDA_HOME}/etc/profile.d/conda.sh" ]; then
